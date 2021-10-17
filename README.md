16bc9cc170ca3144c07223b2f8a047e3c57fa2db
vickyting0910 <vickyting0910@hotmail.com>
2021-10-17 14:50:56 -0500

    updates

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # censusclassificationAPI
+
+## Training Data
+
+Census Income Data Set/ https://archive.ics.uci.edu/ml/datasets/census+income
+
+## Set up Environment
+
+### pip install -r /path/to/requirements.txt
+
+## Attribute Information:
+
+### Predict whether income exceeds $50K/yr based on census data >50K, <=50K.
+
+### age: continuous.
+### workclass: Private, Self-emp-not-inc, Self-emp-inc, Federal-gov, Local-gov, State-gov, Without-pay, Never-worked.
+### fnlwgt: continuous.
+### education: Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, Assoc-voc, 9th, 7th-8th, 12th, Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool.
+### education-num: continuous.
+### marital-status: Married-civ-spouse, Divorced, Never-married, Separated, Widowed, Married-spouse-absent, Married-AF-spouse.
+### occupation: Tech-support, Craft-repair, Other-service, Sales, Exec-managerial, Prof-specialty, Handlers-cleaners, Machine-op-inspct, Adm-clerical, Farming-fishing, Transport-moving, Priv-house-serv, Protective-serv, Armed-Forces.
+### relationship: Wife, Own-child, Husband, Not-in-family, Other-relative, Unmarried.
+### race: White, Asian-Pac-Islander, Amer-Indian-Eskimo, Other, Black.
+### sex: Female, Male.
+### capital-gain: continuous.
+### capital-loss: continuous.
+### hours-per-week: continuous.
+### native-country: United-States, Cambodia, England, Puerto-Rico, Canada, Germany, Outlying-US(Guam-USVI-etc), India, Japan, Greece, South, China, Cuba, Iran, Honduras, Philippines, Italy, Poland, Jamaica, Vietnam, Mexico, Portugal, Ireland, France, Dominican-Republic, Laos, Ecuador, Taiwan, Haiti, Columbia, Hungary, Guatemala, Nicaragua, Scotland, Thailand, Yugoslavia, El-Salvador, Trinadad&Tobago, Peru, Hong, Holand-Netherlands.
+
+
+## API Instruction
+
+### Step1: Type in uvicorn app.main:app --reload in the terminal
+### Step2: Go to http://127.0.0.1:8000/docs
+### Step3: Default/POST/income_prediction
+### Step4: Click Try it out
+### Step5: Enter the following dictionary in Request body
+### Step6: Click Execute
+
+
+{
+  "age": 30,
+  "workclass": "State-gov",
+  "fnlwgt": 77516,
+  "education": "Masters",
+  "education_num": 15,
+  "marital_status": "Never-married",
+  "occupation": "Prof-specialty",
+  "relationship": "Not-in-family",
+  "race": "White",
+  "sex": "Female",
+  "capital_gain": 2174,
+  "capital_loss": 0,
+  "hours_per_week": 40,
+  "native_country": "United-States",
+  "income": 1
+}
+
+### Step7: Got to Server response for results
+
+
---
+++ test_predict.py
@@ -0,0 +1,120 @@
+"""
+Unit Tests for Income Prediction
+
+author: Yan-ting Liau
+date: October 16, 2021
+"""
+import pandas as pd
+from app.predict import incomemodel
+
+model = incomemodel()
+
+
+def test_preprocessing():
+
+    """
+    Test whether the column exists and the data type is correct
+    input:
+            data: pandas dataframe
+    output:
+             None
+    """
+    data, _ = model.preprocessing()
+    required_columns = {
+        "age": pd.api.types.is_integer_dtype,
+        "fnlwgt": pd.api.types.is_integer_dtype,
+        "education_num": pd.api.types.is_integer_dtype,
+        "capital_gain": pd.api.types.is_integer_dtype,
+        "capital_loss": pd.api.types.is_integer_dtype,
+        "hours_per_week": pd.api.types.is_integer_dtype,
+        "income": pd.api.types.is_integer_dtype,
+    }
+
+    # Check columns
+    assert set(data.columns.values).issuperset(set(required_columns.keys()))
+
+    for col_name, format_verification_funct in required_columns.items():
+        assert format_verification_funct(
+            data[col_name]
+        ), f"Column {col_name} failed test {format_verification_funct}"
+
+
+def test_preprocessing_classes():
+    """
+    Test whether the classes exist in the column
+    input:
+            data: pandas dataframe
+    output:
+             None
+    """
+    data, _ = model.preprocessing()
+    # Check that only the known classes are present
+    known_classes = [1, 0]
+
+    assert data["income"].isin(known_classes).all()
+
+
+def test_twoclasses():
+    """
+    Test whether two classes in training data
+    input:
+            data: pandas dataframe
+    output:
+             None
+    """
+    data, _ = model.preprocessing()
+
+    assert len(set(data["income"])) > 1
+
+
+def test_preprocessing_ranges():
+    """
+    Test whether the range of the columnis correct
+    input:
+          data: pandas dataframe
+    output:
+          None
+    """
+    data, _ = model.preprocessing()
+    ranges = {
+            "age": (15, 95),
+            "education_num": (0, 30),
+            "hours_per_week": (0, 100)
+            }
+
+    for col_name, (minimum, maximum) in ranges.items():
+        assert data[col_name].dropna().between(minimum, maximum).all()
+
+
+def test_trainingpred_two_classes():
+    """
+    Test whether two classes are successfully predicted in training data
+    input:
+          data: pandas dataframe
+            training data
+          model.train()
+    output:
+          None
+    """
+    data, _ = model.preprocessing()
+    indep_variable = data.copy()
+    indep_variable.pop("income")
+    classes = model.train().predict(indep_variable)
+    assert len(set(classes)) > 1
+
+
+def test_testingpred_two_classes():
+    """
+    Test whether two classes are successfully predicted in testing data
+    input:
+          data: pandas dataframe
+            testing data
+          model.test_val()
+    output:
+          None
+    """
+    _, data = model.preprocessing()
+    indep_variable = data.copy()
+    indep_variable.pop("income")
+    classes = model.test_val()[0]
+    assert len(set(classes)) > 1
