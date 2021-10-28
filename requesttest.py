e119ea50a21aec6ef172f04c123dc1898e3713be
vickyting0910 <vickyting0910@hotmail.com>
2021-10-28 08:03:03 -0500

    update

--- requesttest.py
+++ requesttest.py
@@ -1,3 +1,9 @@
+import requests
+import json
 
+response = requests.post('https://census-income-pred.herokuapp.com/')
+r = requests.post('https://census-income-pred.herokuapp.com/income_prediction')
 
+print(response.status_code)
+print(response.json())
 
